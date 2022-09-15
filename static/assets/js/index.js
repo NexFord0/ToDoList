eae5cb0b331312cea124cc7887791f89201966eb
NexFord <nexford.bot@gmail.com>
2022-09-15 10:51:19 +0200

    V1.6.1 Removed the search field when interacting with an item.

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -10,6 +10,7 @@ function init() {
         return;
     }
     document.getElementById("list").innerHTML = null;
+    document.getElementById("input-add").value = null;
     for (let element of arr_elements) {
         add_element(element.title, element.valid);
     }
